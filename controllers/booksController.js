d44d595118b96d989a4a188f7abcd11ffd0ad3e2
Scott H <zenwattage@gmail.com>
2019-08-17 00:02:18 -0700

    routes and controllers working

---
+++ controllers/booksController.js
@@ -0,0 +1,42 @@
+const db = require("../models");
+
+//methods for controller
+
+module.exports = {
+    //find
+    findAll : function(req, res){
+        db.Book
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .cath(err => res.status(422).json(err));
+    },
+    findById: function(req,res) {
+        db.Book
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    //create
+    create: function(req, res){
+        db.Book
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    //update
+    update: function(req,res){
+        db.Book
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    //delete
+    remove: function(req,res) {
+        db.Book
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .catch(err => res.status(422).json(err));
+    }
+
+};
