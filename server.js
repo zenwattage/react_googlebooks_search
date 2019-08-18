3b121d2157d66475f1fe0c441a3c127eea288bf1
Scott H <zenwattage@gmail.com>
2019-08-17 20:47:09 -0700

    removed dotenv for if-env

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
