0ce274cdf86b4267448d209ba42e4ad4542b6d70
Scott H <zenwattage@gmail.com>
2019-08-18 15:01:53 -0700

    heroku buildpack set

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import SavedBooks from "./pages/SavedBooks";
+import SaveBooks from "./pages/SaveBooks";
 import SearchBooks from "./pages/SearchBooks";
 import NoResults from "./pages/NoResults";
 import './App.css';
@@ -14,8 +14,8 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" component={ SearchBooks } />
-          <Route exact path="/saved" component={ SavedBooks } />
-          <Route exact path="/saved/:id" component={ SavedBooks } />          
+          <Route exact path="/saved" component={ SaveBooks } />
+          <Route exact path="/saved/:id" component={ SaveBooks } />          
           <Route component={ NoResults } />
         </Switch>
       </div>
